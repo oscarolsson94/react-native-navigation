0fee5afc767d483752bc0982bfa418bcb458f670
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-27 17:15:20 +0200

    setting up stack navigation

--- App.js
+++ App.js
@@ -1,13 +1,21 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import { CategoriesScreen } from "./screens/CategoriesScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <SafeAreaView>
-        <CategoriesScreen />
+        <NavigationContainer>
+          <Stack.Navigator>
+            <Stack.Screen name="MealsCategories" component={CategoriesScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
       </SafeAreaView>
     </>
   );
