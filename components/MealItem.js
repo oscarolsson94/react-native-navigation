2ecd93991590cfebfcaab23a334481943e4a3fe6
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-28 09:18:55 +0200

    styling item

--- components/MealItem.js
+++ components/MealItem.js
@@ -1,10 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, Image } from "react-native";
 import React from "react";
 
-export const MealItem = ({ title }) => {
+export const MealItem = ({ title, imageUrl }) => {
   return (
     <View>
-      <Text>{title}</Text>
+      <Pressable>
+        <View>
+          <Image source={{ uri: imageUrl }} style={styles.image} />
+          <Text style={styles.title}>{title}</Text>
+        </View>
+      </Pressable>
     </View>
   );
 };
+
+const styles = StyleSheet.create({
+  image: {
+    width: "100%",
+    height: 100,
+  },
+  title: {
+    fontWeight: "bold",
+    textAlign: "center",
+    fontSize: 18,
+  },
+});
