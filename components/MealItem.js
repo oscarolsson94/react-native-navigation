6e1815f8c113849790fbea07787e8f468d61d94f
Oscar Olsson <69190482+oscarolsson94@users.noreply.github.com>
2022-10-28 09:32:31 +0200

    styling items

--- components/MealItem.js
+++ components/MealItem.js
@@ -9,16 +9,16 @@ export const MealItem = ({
   affordability,
 }) => {
   return (
-    <View>
+    <View style={styles.mealItem}>
       <Pressable>
         <View>
           <Image source={{ uri: imageUrl }} style={styles.image} />
           <Text style={styles.title}>{title}</Text>
         </View>
-        <View>
-          <Text>{duration}</Text>
-          <Text>{complexity.toUpperCase()}</Text>
-          <Text>{affordability.toUpperCase()}</Text>
+        <View style={styles.details}>
+          <Text style={styles.detailItem}>{duration}</Text>
+          <Text style={styles.detailItem}>{complexity.toUpperCase()}</Text>
+          <Text style={styles.detailItem}>{affordability.toUpperCase()}</Text>
         </View>
       </Pressable>
     </View>
@@ -26,6 +26,12 @@ export const MealItem = ({
 };
 
 const styles = StyleSheet.create({
+  mealItem: {
+    margin: 16,
+    borderRadius: 8,
+    overflow: "hidden",
+    backgroundColor: "white",
+  },
   image: {
     width: "100%",
     height: 100,
@@ -34,5 +40,16 @@ const styles = StyleSheet.create({
     fontWeight: "bold",
     textAlign: "center",
     fontSize: 18,
+    margin: 8,
+  },
+  details: {
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    padding: 8,
+  },
+  detailItem: {
+    marginHorizontal: 4,
+    fontSize: 12,
   },
 });
